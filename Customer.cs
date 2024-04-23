4822c4792a272e7587a40a422f91e4153016854d
Jacob <jacobespo7@yahoo.com>
2024-04-23 14:55:51 -0500

    Create Customer class

---
+++ Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassProject002;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName{ get; set; }
+    public string Passsword{ get; set; }
+    
+
+}
